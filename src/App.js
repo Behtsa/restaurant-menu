0f600aca49c6b0846ac7196fc4972495d3b2de95
Behtsa <behtsabet14@gmail.com>
2018-04-06 11:19:54 -0500

    show breakfast information

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import data from './data';
 //Elements
-import Menu from './components/Menu.js'
-import Order from './components/Order'
+import Menu from './components/Menu.js';
+import Order from './components/Order';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="row">
-        <Menu />
+      <div>
+        <Menu data = {data.breakfast} />
         <Order />
       </div>
     );
