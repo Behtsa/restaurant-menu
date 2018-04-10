d26f9a20d215238485b5dd356cea2a4f30bf1e85
scarlett07 <scarlett.geo@gmail.com>
2018-04-10 14:52:36 -0500

    order sin el total

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,19 +8,24 @@ export default class Order extends Component {
 
     }
 
-    showOrder(){
-        this.props.order.map(item)
-        
-
-    }
     render () {        
-
+        console.log(this.props.order);
+        let { order } = this.props;
+        
         return (
                 <Jumbotron className ="col-md-4 col-lg-4">
                     <h1 className="display-3">Orden</h1>
-                    <p className="lead text-left">{this.props.item}</p>
+                     {order.map(item=>{
+                     return ( <div>
+                                 <p className="lead text-left">{item.item}</p>
+                                 <p className="lead text-right">{item.price}</p>   
+                             </div>) })}
                     <hr className="my-2" />
                     <p className="text-left">TOTAL</p>
+                    {(order && order.lenght > 0 
+                        ? order.reduce((value, order) => value + order.price)
+                        : 0
+                    )}
                     <p className="lead">
                         <Button color="primary col">Enviar Orden</Button>
                     </p>
