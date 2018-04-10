c669d76e2bbfa5c1c69189145b5a87db2150dc2b
Behtsa <behtsabet14@gmail.com>
2018-04-10 12:24:45 -0500

    router

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -6,31 +6,22 @@ export default class Dishes extends Component {
 		constructor(props) {
 			super(props);
 			this.dishes = props.food;
-			this.handleClick = this.handleClick.bind(this);	
+			this.handleClick = this.handleClick.bind(this);
 		}
 
 		handleClick(item, price){
 			Object.assign(this.props.order, { item: item, price: price, quantity: this.props.order.quantity + 1 })
 			console.log(this.props.order);
-			
+
 	}
 
 		render() {
 			console.log(this.props.order);
 			//console.log(this.order);
-			
+
 			return(
 				<div>
 					<Row>
-<<<<<<< HEAD
-              			{this.dishes.map(item => <Card body className = 'col-md-4'>
-                  			<CardImg top width="100%" src={item.src} alt="Card image cap" height="250px" />
-                  			<CardTitle>{item.item}</CardTitle>
-                  			<Button>Agregar</Button>
-                			</Card>
-              			)}
-            		</Row>
-=======
               {this.dishes.map(item => <Card body className = 'col-md-4'>
                   <CardImg top width="100%" src={item.src} alt="Card image cap" height="250px" />
                   <CardTitle>{item.item}</CardTitle>
@@ -38,8 +29,7 @@ export default class Dishes extends Component {
                 </Card>
               )}
             </Row>
->>>>>>> 1365f208345e5cb62c7fe1564b0ddc28036ddbed
+
 				</div>)
 		}
 }
-
