3f1413cc7eb7b02f90ef043794adda079c8de8ff
Behtsa <behtsabet14@gmail.com>
2018-04-11 10:51:31 -0500

    solved conflicts

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,14 +8,6 @@ export default class Order extends Component {
 
     }
 
-<<<<<<< HEAD
-    // showOrder(){
-    //     this.props.order.map(item)
-        
-
-    // }
-=======
->>>>>>> 9cd536c1ee1e4b165095ad7d725c84b40bab1fcd
     render () {        
         console.log(this.props.order);
         let { order } = this.props;
@@ -24,15 +16,11 @@ export default class Order extends Component {
         return (
                 <Jumbotron className ="col-md-4 col-lg-4">
                     <h1 className="display-3">Orden</h1>
-<<<<<<< HEAD
-                    <p className="lead text-left"></p>
-=======
                      {order.map(item=>{
                      return ( <div>
                                  <p className="lead text-left">{item.item}</p>
                                  <p className="lead text-right">{item.price}</p>   
                              </div>) })}
->>>>>>> 9cd536c1ee1e4b165095ad7d725c84b40bab1fcd
                     <hr className="my-2" />
                     <p className="text-left">TOTAL</p>
                     { total.length>0?
