6ea0bdc0c7f1fd7417518c06457bea7ab2de876b
Behtsa <behtsabet14@gmail.com>
2018-04-12 00:24:32 -0500

    Routing DONE

--- src/App.js
+++ src/App.js
@@ -33,56 +33,107 @@ class App extends Component {
           item: "Molletes",
           price: 7,
           src: "https://vips.com.mx/wp-content/uploads/2016/06/0005_454_18_A_Molletes_con_Jamon_0008.jpg.jpg"
-        }],
+          },
+          {
+          item: "Copa de Yogurth",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0015_457_22_Copa_de_Yogurt_0014_A1_E1.jpg.jpg"
+          },
+          {
+          item: "Desayuno Combinado",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0003_494-04-Desayuno-Combinado_0003-E1.jpg.jpg"
+          }],
         lunch: [{
           item: "Enchiladas",
           price: 5,
           src: "https://vips.com.mx/wp-content/uploads/2016/05/0000s_0015_454_34_A_Enchiladas_Suizas_0042.jpg.jpg"
           }, 
           {
-          item: "Pozole",
-          price: 7
+          item: "Tacos Botaneros",
+          price: 7,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/0002__0020_460_12_Tacos_Mixtos_0024_L1.jpg"
           },
           {
-          item: "Birria",
-          price: 10
+          item: "Queso fundido con Chorizo",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0016_473-01-Queso-Fundido-con-Chorizo_0022_A1-E1.jpg.jpg"
           },
           {
-          item: "Sopes",
-          price: 7
-        }],
+          item: "Caldo tlalpeño",
+          price: 7,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0012_472-01-454_25_A_Caldo_Tlalpen%CC%83o_0016-E1.jpg.jpg"
+          },
+          {
+          item: "Ensalada de Pechuga",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/0005__0017_Ensalada-del-Chef-con-pechuga-1.jpg"
+          },
+          {
+          item: "Parrillada",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/parrillada.jpg"
+          }],
         hamburguers: [{
-          item: "Queso & Tocino",
-          price: 5
+          item: "Hamburguesa Champions",
+          price: 5,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/champions.jpg"
           }, 
           {
-          item: "Pollo",
-          price: 7
+          item: "Hamburguesa Hawaina",
+          price: 7,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0018_473-03-Hamburguesa-Hawaiana_0014_A1-E1.jpg.jpg"
           },
           {
-          item: "Vegetariana",
-          price: 10
+          item: "Hamburguesa de Pollo",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/pollo-emp.jpg"
           },
           {
-          item: "Pollo & Jamón",
-          price: 7
-        }],
+          item: "Habanero Crunch",
+          price: 7,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0019_473-04-Hamburguesa-Habanero-Cronch_0009-A1-E1.jpg.jpg"
+          },
+          {
+          item: "Hamburguesa Pechuga",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/pollo-emp.jpg"
+          },
+          {
+          item: "Hamburguesa Mexicana",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0017_473-02-Hamburguesa-Mexicana_0017_A1-E1.jpg.jpg"
+          }],
         beverages: [{
-          item: "Café americano",
-          price: 5
+          item: "Capuccino Clásico",
+          price: 5,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/clasicoi.jpg"
           }, 
           {
-          item: "Café con leche",
-          price: 7
+          item: "Capuccino Moka",
+          price: 7,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/moka.jpg"
           },
           {
-          item: "Agua",
-          price: 10
+          item: "Limonadas frutales",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2016/05/1_0000s_0007_491-14-15-16-Limonadas_0025_L1-E1.jpg.jpg"
           },
           {
-          item: "Jugo natural",
-          price: 7
-        }]
+          item: "Cerveza",
+          price: 7,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/cerveza-2.jpg"
+          },
+          {
+          item: "Espresso Americano",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/espresso.jpg"
+          },
+          {
+          item: "Té de Hoja Suelta",
+          price: 10,
+          src: "https://vips.com.mx/wp-content/uploads/2018/01/teorganico.jpg"
+          }]
       }]
     };
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import classnames from 'classnames';
 import Dishes from './Dishes.js'
-import Test from './Test'
+import '../Menu.css';
 import { Route, Switch, Link } from 'react-router-dom';
 /*
 <Route exact path='/breakfast' render = {() => {
@@ -23,16 +23,16 @@ export default class Menu extends Component {
       <div className="col-lg-8">
         <Nav tabs className="row">
           <NavItem className="col-lg-3">
-              <Link to='/breakfast'>Desayuno</Link>
+              <Link to='/breakfast' className="link">Desayuno</Link>
           </NavItem>
           <NavItem className="col-lg-3">
-              <Link to='/meals'>Comida</Link>
+              <Link to='/meals' className="link">Comida</Link>
           </NavItem>
           <NavItem className="col-lg-3">
-              <Link to='/hamburguers'>Hamburguesas</Link>
+              <Link to='/hamburguers' className="link">Hamburguesas</Link>
           </NavItem>
           <NavItem className="col-lg-3">
-              <Link to='/beverages'>Bebidas</Link>
+              <Link to='/beverages' className="link">Bebidas</Link>
           </NavItem>
         </Nav>
   
@@ -41,13 +41,13 @@ export default class Menu extends Component {
             return <Dishes order={this.props.order} upDateOrder={this.props.upDateOrder} food = {this.breakfast} />
           }} />
           <Route exact path='/meals' render = {() => {
-            return <Dishes order={this.props.order} food={this.lunch} />
+            return <Dishes order={this.props.order} upDateOrder={this.props.upDateOrder} food={this.lunch} />
           }} />
           <Route exact path='/hamburguers' render = {() => {
-            return <Dishes order={this.props.order} food = {this.hamburguers} />
+            return <Dishes order={this.props.order} upDateOrder={this.props.upDateOrder} food = {this.hamburguers} />
           }} />
           <Route exact path='/beverages' render = {() => {
-            return <Dishes order={this.props.order} food = {this.beverages} />
+            return <Dishes order={this.props.order} upDateOrder={this.props.upDateOrder} food = {this.beverages} />
           }} />
         </Switch>
       </div>
